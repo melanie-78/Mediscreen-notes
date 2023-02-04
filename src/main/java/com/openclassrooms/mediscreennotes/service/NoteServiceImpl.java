0f0157122a7a30e9135143c27a9ef822d6233bc9
Mel <adjohmelanie@gmail.com>
2023-02-04 22:53:54 +0100

    Patient notes management

--- src/main/java/com/openclassrooms/mediscreennotes/service/NoteServiceImpl.java
+++ src/main/java/com/openclassrooms/mediscreennotes/service/NoteServiceImpl.java
@@ -17,6 +17,11 @@ public class NoteServiceImpl implements NoteService{
     private NoteRepository noteRepository;
     private NoteMapper dtoNote;
 
+    /**
+     *this permits to insert a note in database
+     * @param noteDTO data of note
+     * @return noteDTO saved
+     */
     @Override
     public NoteDTO addNote(NoteDTO noteDTO) {
         Note note = dtoNote.fromNoteDTO(noteDTO);
@@ -25,7 +30,10 @@ public class NoteServiceImpl implements NoteService{
         return  dtoNote.fromNote(insert);
     }
 
-
+    /**
+     * @param id is the identifier of a note
+     * @return data about note identified by id
+     */
     @Override
     public NoteDTO getNote(String id) {
         Note noteById = noteRepository.findById(id)
@@ -33,6 +41,10 @@ public class NoteServiceImpl implements NoteService{
         return dtoNote.fromNote(noteById);
     }
 
+    /**
+     *
+     * @return a list of data about notes saved in database
+     */
     @Override
     public List<NoteDTO> getAllNote() {
         List<Note> all = noteRepository.findAll();
@@ -43,6 +55,10 @@ public class NoteServiceImpl implements NoteService{
         return allNotes;
     }
 
+    /**
+     * this permits to delete a note by id
+     * @param id the identifier of the note
+     */
     @Override
     public void deleteNote(String id) {
         Note note = noteRepository.findById(id)
@@ -50,6 +66,11 @@ public class NoteServiceImpl implements NoteService{
         noteRepository.delete(note);
     }
 
+    /**
+     *
+     * @param patId is the identifier's patient
+     * @return a list of noteDTO of this patient
+     */
     @Override
     public List<NoteDTO> getAllNotePatient(int patId) {
         List<Note> notes = noteRepository.findByPatId(patId);
@@ -61,6 +82,12 @@ public class NoteServiceImpl implements NoteService{
         return noteDTOs;
     }
 
+    /**
+     * this permits to update note's data
+     * @param id is the identifier of a patient
+     * @param noteDTO data about note identified by id
+     * @return noteDTO updated
+     */
     @Override
     public NoteDTO update(String id, NoteDTO noteDTO) {
 
